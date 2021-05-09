97696e8e1aa0cf5a4660756022c8ea619848e1f4
Boaz Lowenstein <odin@curve.tech>
2021-05-09 08:59:31 +0300

    new docker file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Its ODIN');
+  res.send('Hello World! Its ODINUX test tag');
 });
 
 var server = app.listen(3000, function () {
